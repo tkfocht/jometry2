e5d4d61f442fa6ed84bcb65d9ce888d2447b568f
TK Focht <tkfocht@alum.mit.edu>
2023-04-01 20:18:55 -0700

    Include vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,20 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        game: resolve(__dirname, 'game.html'),
-        contestant: resolve(__dirname, 'contestant.html'),
-        period: resolve(__dirname, 'period.html'),
-        contestantIndex: resolve(__dirname, 'contestant_index.html'),
-        about: resolve(__dirname, 'about.html'),
-        glossary: resolve(__dirname, 'glossary.html'),
-        blog: resolve(__dirname, 'blog.html')
-      }
     }
   }
 })
