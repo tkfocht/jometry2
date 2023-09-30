fd0df19e71bf94b673f62464ede92972935e1b59
TK Focht <tkfocht@alum.mit.edu>
2023-09-29 22:30:20 -0700

    Remove unused components

--- src/components/util/ReactiveChart.vue
+++ src/components/util/ReactiveChart.vue
@@ -30,7 +30,6 @@ watch(() => props.chart, (newValue, oldValue) => {
 @import 'bootstrap/scss/mixins';
 
 div.graph {
-    width: min(95vw, 900px);
     --bs-aspect-ratio: 100%;
     @include media-breakpoint-up(sm) {
         --bs-aspect-ratio: calc(9 / 16 * 100%);
