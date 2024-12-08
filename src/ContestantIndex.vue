09e660421978c9a9edb00ab27789cc4f0a691665
TK Focht <tkfocht@alum.mit.edu>
2024-12-08 00:37:32 -0800

    Team List for pcj

--- src/ContestantIndex.vue
+++ src/ContestantIndex.vue
@@ -1,17 +1,32 @@
 <script setup>
 import * as d3 from 'd3'
 import * as data from '@/data'
-import { filterValues } from '@/util'
+import { filterValues, isPopCulture, subdomainIdentifier } from '@/util'
 import Footer from './components/Footer.vue'
 import Header from './components/Header.vue'
 
+
+const displayAsTeamListing = window.location.pathname === '/team_index.html'
+
 data.loadContestantData()
 data.loadGameContestantStatData()
+if (isPopCulture() && displayAsTeamListing) {
+  data.loadTeamData()
+  data.loadGameTeamStatData()
+}
 
 const gameContestantStatDataByContestantId = data.gameContestantStatDataByContestantId
+const gameTeamStatDataByTeamId = data.gameTeamStatDataByTeamId
 
 const contestantsByName = data.computedIfRefHasValue(data.contestantData, cData => d3.group(cData, c => c.name))
 const duplicateContestants = data.computedIfRefHasValue(contestantsByName, cData => {
+  console.log(cData)
+  cData = filterValues(cData, v => v.length > 1)
+  return cData
+})
+
+const teamsByName = data.computedIfRefHasValue(data.teamData, cData => d3.group(cData, c => c.name))
+const duplicateTeams = data.computedIfRefHasValue(teamsByName, cData => {
   console.log(cData)
   cData = filterValues(cData, v => v.length > 1)
   return cData
@@ -26,17 +41,35 @@ const contestantData = data.computedIfRefHasValues(
   }
 )
 
+const teamData = data.computedIfRefHasValues(
+  [data.teamData, gameTeamStatDataByTeamId],
+  (cData, gcsData) => {
+    cData = cData.filter(c => gcsData.get(c.team_id)?.length > 0)
+    cData.sort((a, b) => d3.ascending(a.name, b.name))
+    return cData
+  }
+)
+
 </script>
 
 <template>
   <Header />
-  <div class="component-body">
-    <div class="section" v-if="contestantData && gameContestantStatDataByContestantId">
+  <div class="component-body" :data-bs-theme="subdomainIdentifier()">
+    <div class="section" v-if="!displayAsTeamListing && contestantData && gameContestantStatDataByContestantId">
       <div class="section-header">Contestant Index</div>
       <div v-for="c in contestantData">
         <a :href="'/contestant.html?data_source=standard&contestant_id=' + c.contestant_id">{{ c.name }}</a>&nbsp;
         <span v-if="gameContestantStatDataByContestantId.get(c.contestant_id)?.length > 0">
           {{ gameContestantStatDataByContestantId.get(c.contestant_id)?.length }} game<template v-if="gameContestantStatDataByContestantId.get(c.contestant_id)?.length > 1">s</template>
+        </span>
+      </div>
+    </div>
+    <div class="section" v-if="displayAsTeamListing && teamData && gameTeamStatDataByTeamId">
+      <div class="section-header">Team Index</div>
+      <div v-for="c in teamData">
+        <a :href="'/team.html?data_source=standard&team_id=' + c.team_id">{{ c.name }}</a>&nbsp;
+        <span v-if="gameTeamStatDataByTeamId.get(c.team_id)?.length > 0">
+          {{ gameTeamStatDataByTeamId.get(c.team_id)?.length }} game<template v-if="gameTeamStatDataByTeamId.get(c.team_id)?.length > 1">s</template>
         </span>
       </div>
     </div>
